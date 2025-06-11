ddc26b22d66a4fc063746805910d4096648bc61b
raindrive100 <raindrive100@gmail.com>
2025-06-11 12:40:45 -0400

    Removed print statements to the logger.

--- src/rameshm/llmeng/exception/llm_chat_exception.py
+++ src/rameshm/llmeng/exception/llm_chat_exception.py
@@ -1,8 +1,8 @@
-from rameshm.llmeng.utils.init_utils import set_environment_logger
+#from rameshm.llmeng.utils.init_utils import set_environment_logger
 
 class LlmChatException(Exception):
     """Base class for all exceptions in the LLM chat module."""
     def __init__(self, message: str):
         super().__init__(message)
-        self.logger = set_environment_logger()
-        self.logger.error(message)
+        #self.logger = set_environment_logger()
+        #self.logger.error(message)
