fffe9bbcd480a76e8ed3b8185a18d1dc36d4be87
raindrive100 <raindrive100@gmail.com>
2025-06-14 16:22:09 -0400

    Added a few more constants to indicate which models support images, images in PDF, and PDFs.

--- src/rameshm/llmeng/utils/set_environment.py
+++ src/rameshm/llmeng/utils/set_environment.py
@@ -19,7 +19,12 @@ def set_my_environment():
     load_dotenv(key_file)
 
     os.environ.update({
+        # gemini-2.5-pro is not supported for API version, hence excluded.
         "MAX_SIZE_OF_FILE_UPLOADS": str(1024*1024*20),   # 20MB max total size of all uploaded files.
+        "FILE_DETECTION_CONFIDENCE_LEVEL_NEEDED": "0.6",
+        "MULTI_MODAL_MODELS": ", ".join(["gpt-4o", "gpt-4o-mini", "claude-sonnet-4-0", "gemini-1.5-flash"]), # gemma:4B can process images but not including any models that run locally.
+        "PDF_IMAGE_HANDLING_MODELS": ", ".join(["claude-sonnet-4-0", "gemini-1.5-flash"]),
+        "IMAGE_HANDLING_MODELS": ", ".join(["gpt-4o", "gpt-4o-mini", "claude-sonnet-4-0", "gemini-1.5-flash"]),
         "LLM_MY_LOGGER_NAME": "llm_engineering",
         "LLM_LOG_TO_FILE": "True",
         "LLM_LOG_DIR": "c:\\temp",
