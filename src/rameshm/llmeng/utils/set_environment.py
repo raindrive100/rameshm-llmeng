827cf004158303a01e2342959543d0bbec14e5f5
raindrive100 <raindrive100@gmail.com>
2025-06-16 23:15:32 -0400

    1. Removed all constants from Environment and moved to chat_constants. THis is a major change.

--- src/rameshm/llmeng/utils/set_environment.py
+++ src/rameshm/llmeng/utils/set_environment.py
@@ -7,8 +7,6 @@ Loads environment variables for the llm_engineering project.
 """
 # imports external and system packages
 import os
-
-# import internal packages
 from dotenv import load_dotenv
 
 def set_my_environment():
@@ -20,11 +18,11 @@ def set_my_environment():
 
     os.environ.update({
         # gemini-2.5-pro is not supported for API version, hence excluded.
-        "MAX_SIZE_OF_FILE_UPLOADS": str(1024*1024*20),   # 20MB max total size of all uploaded files.
-        "FILE_DETECTION_CONFIDENCE_LEVEL_NEEDED": "0.6",
-        "MULTI_MODAL_MODELS": ", ".join(["gpt-4o", "gpt-4o-mini", "claude-sonnet-4-0", "gemini-1.5-flash"]), # gemma:4B can process images but not including any models that run locally.
-        "PDF_IMAGE_HANDLING_MODELS": ", ".join(["claude-sonnet-4-0", "gemini-1.5-flash"]),
-        "IMAGE_HANDLING_MODELS": ", ".join(["gpt-4o", "gpt-4o-mini", "claude-sonnet-4-0", "gemini-1.5-flash"]),
+        #"MAX_SIZE_OF_FILE_UPLOADS": str(1024*1024*20),   # 20MB max total size of all uploaded files.
+        #"FILE_DETECTION_CONFIDENCE_LEVEL_NEEDED": "0.6",
+        #"MULTI_MODAL_MODELS": ", ".join(["gpt-4o", "gpt-4o-mini", "claude-sonnet-4-0", "gemini-1.5-flash"]), # gemma:4B can process images but not including any models that run locally.
+        #"PDF_IMAGE_HANDLING_MODELS": ", ".join(["claude-sonnet-4-0", "gemini-1.5-flash"]),
+        #"IMAGE_HANDLING_MODELS": ", ".join(["gpt-4o", "gpt-4o-mini", "claude-sonnet-4-0", "gemini-1.5-flash"]),
         "LLM_MY_LOGGER_NAME": "llm_engineering",
         "LLM_LOG_TO_FILE": "True",
         "LLM_LOG_DIR": "c:\\temp",
