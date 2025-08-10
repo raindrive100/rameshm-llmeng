0555519bd2a77820ac625377074a54226d690159
raindrive100 <raindrive100@gmail.com>
2025-08-10 17:40:31 -0400

    Make it into YAML file rather than a .PY file. Cange it later.h

---
+++ src/rameshm/llmeng/rag/rag_constants.py
@@ -0,0 +1,6 @@
+RAG_EMBEDDING_DIMENSION = 1053 # Dimension of the embeddings used in the RAG system
+RAG_EMBEDDING_MODEL_GEMINI = "gemini-embedding-001"  # Gemini embedding model
+RAG_EMBEDDING_MODEL_OPENAI = "text-embedding-3-small"  # OpenAI embedding model
+RAG_LLM_MODEL_GEMINI = "gemini-1.5-flash"  # Default RAG LLM model for Gemini
+RAG_LLM_MODEL_OPENAI = "gpt-4o-mini"  # Default
+RAG_VECTOR_STORE = "FAISS"
